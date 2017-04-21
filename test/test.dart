73f9d634b44919129419b4b47e388d37151da32e
P. Hotz <p.hotz@posteo.net>
2017-04-21 08:05:12 +0200

    Modify Model so that it can copy vertex positions and normals into a single Float32List object

--- test/test.dart
+++ test/test.dart
@@ -1,183 +1,223 @@
+import 'package:vector_math/vector_math.dart';
 import "dart:typed_data";
 import "package:test/test.dart";
 import '../web/model.dart';
+import 'dart:io';
 
 void main() {
-  test("Reads an object's faces", () {
-    Model m = new Model.fromObj("""
-# OBJ file created by ply_to_obj.c
-#
-g Object001
+//   test("Reads an object's faces", () {
+//     Model m = new Model.fromObj("""
+// # OBJ file created by ply_to_obj.c
+// #
+// g Object001
 
-v  0  0  0
-v  1  0  0
-v  1  1  0
-v  0  1  0
-v  0.5  0.5  1.6
+// v  0  0  0
+// v  1  0  0
+// v  1  1  0
+// v  0  1  0
+// v  0.5  0.5  1.6
 
-f  5  2  3
-f  4  5  3
-f  6  3  2
-f  5  6  2
-f  4  6  5
-f  6  4  3
-""");
+// f  5  2  3
+// f  4  5  3
+// f  6  3  2
+// f  5  6  2
+// f  4  6  5
+// f  6  4  3
+// """);
 
-    expect(m.faces.length, equals(6));
-    expect(m.faces.first, equals([5, 2, 3]));
-    expect(m.faces.last, equals([6, 4, 3]));
-  });
+//     expect(m.faces.length, equals(6));
+//     expect(m.faces.first, equals([4, 1, 2]));
+//     expect(m.faces.last, equals([5, 3, 2]));
+//   });
 
-  test("Knows an object's vertices", () {
-    Model m = new Model.fromObj("""
-# OBJ file created by ply_to_obj.c
-#
-g Object001
+//   test("Knows an object's vertices", () {
+//     Model m = new Model.fromObj("""
+// # OBJ file created by ply_to_obj.c
+// #
+// g Object001
 
-v  0  0  0
-v  1  0  0
-v  1  1  0
-v  0  1  0
-v  0.5  0.5  1.6
+// v  0  0  0
+// v  1  0  0
+// v  1  1  0
+// v  0  1  0
+// v  0.5  0.5  1.6
 
-f  5  2  3
-f  4  5  3
-f  6  3  2
-f  5  6  2
-f  4  6  5
-f  6  4  3
-""");
+// f  5  2  3
+// f  4  5  3
+// f  6  3  2
+// f  5  6  2
+// f  4  6  5
+// f  6  4  3
+// """);
 
-    expect(m.vertices.length, equals(5));
+//     expect(m.vertices.length, equals(5));
 
-    List<double> arr = new List<double>(3);
+//     List<double> arr = new List<double>(3);
 
-    m.vertices.first.copyIntoArray(arr);
-    expect(arr, equals([0.0, 0.0, 0.0]));
+//     m.vertices.first.copyIntoArray(arr);
+//     expect(arr, equals([0.0, 0.0, 0.0]));
 
-    m.vertices.last.copyIntoArray(arr);
-    expect(arr, equals([0.5, 0.5, closeTo(1.6, 0.00001)]));
-  });
+//     m.vertices.last.copyIntoArray(arr);
+//     expect(arr, equals([0.5, 0.5, closeTo(1.6, 0.00001)]));
+//   });
 
-  test("vertices", () {
-    Model m = new Model.fromObj("""
-# diamond.obj
+//   test("vertices", () {
+//     Model m = new Model.fromObj("""
+// # diamond.obj
 
-g Object001
+// g Object001
 
-v 0.000000E+00 0.000000E+00 78.0000
-v 45.0000 45.0000 0.000000E+00
-v 45.0000 -45.0000 0.000000E+00
-v -45.0000 -45.0000 0.000000E+00
-v -45.0000 45.0000 0.000000E+00
-v 0.000000E+00 0.000000E+00 -78.0000
+// v 0.000000E+00 0.000000E+00 78.0000
+// v 45.0000 45.0000 0.000000E+00
+// v 45.0000 -45.0000 0.000000E+00
+// v -45.0000 -45.0000 0.000000E+00
+// v -45.0000 45.0000 0.000000E+00
+// v 0.000000E+00 0.000000E+00 -78.0000
 
-f     1 2 3
-f     1 3 4
-f     1 4 5
-f     1 5 2
-f     6 5 4
-f     6 4 3
-f     6 3 2
-f     6 2 1
-f     6 1 5
-""");
+// f     1 2 3
+// f     1 3 4
+// f     1 4 5
+// f     1 5 2
+// f     6 5 4
+// f     6 4 3
+// f     6 3 2
+// f     6 2 1
+// f     6 1 5
+// """);
 
-    List<double> arr = new List(3);
+//     List<double> arr = new List(3);
 
-    m.vertices.first.copyIntoArray(arr);
-    expect(arr, equals([0, 0, 78]));
+//     m.vertices.first.copyIntoArray(arr);
+//     expect(arr, equals([0, 0, 78]));
 
-    m.vertices.last.copyIntoArray(arr);
-    expect(arr, equals([0, 0, -78]));
+//     m.vertices.last.copyIntoArray(arr);
+//     expect(arr, equals([0, 0, -78]));
 
-    expect(m.faces.first, equals([1, 2, 3]));
-    expect(m.faces.last, equals([6, 1, 5]));
-  });
+//     expect(m.faces.first, equals([0, 1, 2]));
+//     expect(m.faces.last, equals([5, 0, 4]));
+//   });
 
-  test("vertices as a Float32List", () {
-    Model m = new Model.fromObj("""
-# OBJ file created by ply_to_obj.c
-#
-g Object001
+//   test("vertices as a Float32List", () {
+//     Model m = new Model.fromObj("""
+// # OBJ file created by ply_to_obj.c
+// #
+// g Object001
 
-v  0  -0.525731  0.850651
-v  0.850651  0  0.525731
-v  0.850651  0  -0.525731
-v  -0.850651  0  -0.525731
-v  -0.850651  0  0.525731
-v  -0.525731  0.850651  0
-v  0.525731  0.850651  0
-v  0.525731  -0.850651  0
-v  -0.525731  -0.850651  0
-v  0  -0.525731  -0.850651
-v  0  0.525731  -0.850651
-v  0  0.525731  0.850651
+// v  0  -0.525731  0.850651
+// v  0.850651  0  0.525731
+// v  0.850651  0  -0.525731
+// v  -0.850651  0  -0.525731
+// v  -0.850651  0  0.525731
+// v  -0.525731  0.850651  0
+// v  0.525731  0.850651  0
+// v  0.525731  -0.850651  0
+// v  -0.525731  -0.850651  0
+// v  0  -0.525731  -0.850651
+// v  0  0.525731  -0.850651
+// v  0  0.525731  0.850651
 
-f  2  3  7
-f  2  8  3
-f  4  5  6
-f  5  4  9
-f  7  6  12
-f  6  7  11
-f  10  11  3
-f  11  10  4
-f  8  9  10
-f  9  8  1
-f  12  1  2
-f  1  12  5
-f  7  3  11
-f  2  7  12
-f  4  6  11
-f  6  5  12
-f  3  8  10
-f  8  2  1
-f  4  10  9
-f  5  9  1
-""");
+// f  2  3  7
+// f  2  8  3
+// f  4  5  6
+// f  5  4  9
+// f  7  6  12
+// f  6  7  11
+// f  10  11  3
+// f  11  10  4
+// f  8  9  10
+// f  9  8  1
+// f  12  1  2
+// f  1  12  5
+// f  7  3  11
+// f  2  7  12
+// f  4  6  11
+// f  6  5  12
+// f  3  8  10
+// f  8  2  1
+// f  4  10  9
+// f  5  9  1
+// """);
 
-    Float32List v = m.verticesAsArr();
+//     Float32List v = m.verticesAsArr();
     
-    const int numberOfVertices = 12;
+//     const int numberOfVertices = 12;
 
-    expect(v.elementSizeInBytes, equals(4));
-    expect(v.buffer.lengthInBytes,
-        equals(4 * numberOfVertices * 3));
+//     expect(v.elementSizeInBytes, equals(4));
+//     expect(v.buffer.lengthInBytes,
+//         equals(4 * numberOfVertices * 3));
 
-    expect(v.length, equals(3 * numberOfVertices));
-    expect(v[0], equals(0));
-    expect(v.last, closeTo(0.850651, 0.001));
+//     expect(v.length, equals(3 * numberOfVertices));
+//     expect(v[0], equals(0));
+//     expect(v.last, closeTo(0.850651, 0.001));
+//   });
+
+//   test("", () {
+//     Model m = new Model.fromObj("""
+// # OBJ file created by ply_to_obj.c
+// #
+// g Object001
+
+// v  1  0  0
+// v  0  -1  0
+// v  -1  0  0
+// v  0  1  0
+// v  0  0  1
+// v  0  0  -1
+
+// f  2  1  5
+// f  3  2  5
+// f  4  3  5
+// f  1  4  5
+// f  1  2  6
+// f  2  3  6
+// f  3  4  6
+// f  4  1  6
+// """);
+
+//     Uint8List indices = m.facesAsArr();
+
+//     expect(indices.length, equals(3 * 8));
+//     expect(indices.buffer.lengthInBytes, equals(3 * 8));
+//     expect(indices.first, equals(1));
+//     expect(indices.last, equals(5));
+//   });
+
+  test("vertex normals", () {
+    var f = new File('models/cube.obj');
+
+    String cube = f.readAsStringSync();
+    Model m = new Model.fromObj(cube);
+
+    var t = m.triangles[1];
+    print('triangle: ' + t.toString());
+
+    Vertex v = t.vertices[0];
+    Vector3 n = v.normal;
+
+    expect(n.x, equals(0));
+    expect(n.y, equals(0));
+    expect(n.z, equals(-1));
+
+    t = m.triangles.last;
+    v = t.vertices.first;
+    n = v.normal;
+
+    expect(n.x, equals(0));
+    expect(n.y, equals(0));
+    expect(n.z, equals(1));
   });
 
-  test("", () {
-    Model m = new Model.fromObj("""
-# OBJ file created by ply_to_obj.c
-#
-g Object001
 
-v  1  0  0
-v  0  -1  0
-v  -1  0  0
-v  0  1  0
-v  0  0  1
-v  0  0  -1
+  test("buffer containing both position vectors and normals", () {
+    var f = new File('models/cube.obj');
 
-f  2  1  5
-f  3  2  5
-f  4  3  5
-f  1  4  5
-f  1  2  6
-f  2  3  6
-f  3  4  6
-f  4  1  6
-""");
+    String cube = f.readAsStringSync();
+    Model m = new Model.fromObj(cube);
 
-    Uint8List indices = m.facesAsArr();
+    Float32List l = m.positionsAndNormalsToArr();
 
-    expect(indices.length, equals(3 * 8));
-    expect(indices.buffer.lengthInBytes, equals(3 * 8));
-    expect(indices.first, equals(2));
-    expect(indices.last, equals(6));
+
   });
 
+  
 }
--- web/model.dart
+++ web/model.dart
@@ -1,66 +1,74 @@
 import 'package:vector_math/vector_math.dart';
 import 'dart:typed_data';
 
-class Model {
-  List<Vector3> _vertices;
-  List<List<int>> _faces;
 
-  List<Vector3> get vertices => _vertices;
-  List<List<int>> get faces => _faces;
-  
+class Vertex {
+  final Model _model;
+  int vertexIndex;
+  int normalIndex;
+
+  Vector3 get position => this._model.positions[this.vertexIndex];
+  Vector3 get normal => this._model.normals[this.normalIndex];
+
+  Vertex(Model model, int vertexIndex, int normalIndex)
+    : _model = model
+  {
+    this.vertexIndex = vertexIndex;
+    this.normalIndex = normalIndex;
+  }
+}
+
+class Triangle {
+  List<Vertex> _vertices;
+
+  List<Vertex> get vertices => _vertices;
+
+  Triangle(List<Vertex> _vertices) {
+    if (null == _vertices) {
+      throw new Exception('no vertices provided');
+    }
+    this._vertices = _vertices;
+  }
+
+  String toString() {
+    return this._vertices
+      .map((v) => (v.vertexIndex + 1).toString() + '//' + (v.normalIndex + 1).toString())
+      .join(' ');
+  }
+}
+
+class Model {
+  List<Vector3> _positions;
+  List<Triangle> _triangles;
+  List<Vector3> _normals;
+
+  List<Vector3> get positions => _positions;
+  List<Vector3> get normals => _normals;
+  List<Triangle> get triangles => _triangles;
 
   Model.fromObj(String objFile) {
 
     List<String> lines = objFile.split("\n");
 
-    this._faces = new List.from(lines.where(_isFaceDef).map(_faceDefToFace));
+    this._positions = new List.from(lines.where(_isVertexDef).map(_vertexDefToVertex));
 
-    this._vertices = new List.from(lines.where(_isVertexDef).map(_vertexDefToVertex));
-
-  }
+    this._normals = new List.from(lines.where(_isNormalDef).map(_normalDefToNormal));
 
 
-  Float32List verticesAsArr() {
-    const l = 3;
 
-    Float32List arr = new Float32List(l * this._vertices.length);
-
-    int i = 0;
-
-    for (Vector3 v in this._vertices) {
-      v.copyIntoArray(arr, i * l);
-      i++;
-    }
-
-    return arr;
-  }
-
-  Uint8List facesAsArr() {
-    const int l = 3;
-
-    Uint8List arr = new Uint8List(l * this._faces.length);
-
-    int i = 0;
-
-    for (List<int> face in this._faces) {
-      arr.setAll(l * i, face);
-      i++;
-    }
-
-    return arr;
+    this._triangles = new List.from(lines.where(_isFaceDef).map(this._faceDefToFace));
   }
 
   static bool _isVertexDef(String line) {
     return line.startsWith("v ");
   }
 
-  static bool _isFaceDef(String line) {
-    return line.startsWith("f ");
+  static bool _isNormalDef(String line) {
+    return line.startsWith("vn ");
   }
 
-  static List<int> _faceDefToFace(String faceDef) {
-    RegExp e = new RegExp(r" +");
-    return new List.from(faceDef.split(e).skip(1).map(int.parse));
+  static bool _isFaceDef(String line) {
+    return line.startsWith("f ");
   }
 
   static Vector3 _vertexDefToVertex(String vertexDef) {
@@ -70,4 +78,70 @@ class Model {
     return new Vector3.array(components);
   }
 
+  static Vector3 _normalDefToNormal(String normalDef) {
+    RegExp e = new RegExp(r" +");
+    List<double> components = new List.from(normalDef.split(e).skip(1).map(double.parse));
+
+    return new Vector3.array(components);
+  }
+
+  Triangle _faceDefToFace(String faceDef) {
+    RegExp e = new RegExp(r" +");
+
+    List<Vertex> vertices = new List.from(faceDef.trim().split(e).skip(1)
+        .map(this._vertexDefToVertex2));
+
+    Triangle triangle = new Triangle(vertices);
+
+    return triangle;
+  }
+
+  Vertex _vertexDefToVertex2(String vertexDef) {
+    List<String> constituents = vertexDef.split("//");
+
+    int vertexIndex = int.parse(constituents.first) - 1;
+    int normalIndex = int.parse(constituents.last) - 1;
+
+    Vertex vertex = new Vertex(this, vertexIndex, normalIndex);
+
+    return vertex;
+  }
+
+  // constraint:
+  // for all vertices v, u
+  // such that v and u have the same index in the index buffer
+  // v and u also have the same position and the same normal
+
+
+  Float32List positionsAndNormalsToArr() {
+    const int floatsPerTriangle = 3 * 3 * 2;
+    const int floatsPerVertex = 3 * 2;
+    const int floatsPerVector = 3;
+    int vertices = 3 * this._triangles.length;
+    int elements = 6 * vertices;
+    Float32List list = new Float32List(elements);
+
+    int triangleIndex = 0;
+    for (Triangle triangle in this._triangles) {
+
+      int vertexIndex = 0;
+      for (Vertex vertex in triangle.vertices) {
+
+        int offset = triangleIndex * floatsPerTriangle
+          + vertexIndex * floatsPerVertex;
+
+        vertex.position.copyIntoArray(list, offset);
+        vertex.normal.copyIntoArray(list, offset + floatsPerVector);
+        
+        vertexIndex++;
+      }
+
+      triangleIndex++;
+    }
+
+    return list;
+  }
+  
+
 }
+
